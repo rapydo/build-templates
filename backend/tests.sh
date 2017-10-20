370dbf76f17373105181d758f3e82f37a4494036
Mattia D'Antonio <m.dantonio@cineca.it>
2017-10-20 14:11:23 +0200

    Extended coverage

--- backend/tests.sh
+++ backend/tests.sh
@@ -3,12 +3,14 @@ set -e
 
 if [ -z "$1" ]; then
     CURRENT_PACKAGE="$VANILLA_PACKAGE"
+    COV="--cov=$CURRENT_PACKAGE.apis --cov=$CURRENT_PACKAGE.tasks --cov=$CURRENT_PACKAGE.models"
 else
     CURRENT_PACKAGE=$1
+    COV="--cov=$CURRENT_PACKAGE"
 fi
 
 echo "Launching unittests with coverage"
 echo "Package: $CURRENT_PACKAGE"
 sleep 1
 
-py.test --confcutdir=tests -x -s --cov=$CURRENT_PACKAGE tests
+py.test --confcutdir=tests -x -s $COV tests
