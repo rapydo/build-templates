25dfea1a8f3720735539015683634c8aa8b7909f
Mattia <m.dantonio81@gmail.com>
2018-02-03 19:59:32 +0100

    Removed failed tests with no-bin-link option

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -13,7 +13,13 @@ node /rapydo/utility/merge.js
 # --production to install only dependencies e not devDependencies
 npm --prefix $MODULE_PATH install
 
-#npm cache clean
+# npm --prefix $MODULE_PATH install --no-bin-links
+# why no-bin-links? MODULE_PATH is mounted from the host machine and this can
+# modify how sym-links can be followed leading to problems at runtime,
+# for example: throw new Error('"' + aSource + '" is not in the SourceMap.');
+# during the build
+
+# npm cache clean
 
 export PATH=$PATH:$MODULE_PATH/node_modules/.bin
 export NODE_PATH=$MODULE_PATH/node_modules
