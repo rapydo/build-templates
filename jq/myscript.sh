f45ad407525f6c1f19e43b6b8fc68d05370d75db
Paolo D <pdonorio@users.noreply.github.com>
2018-04-26 12:17:11 +0200

    long due update (#11)
    * Remove unsupported 17.04 ubuntu
    
    * Upgraded unsupported ubuntu 17.04 to 17.10
    
    * Upgrade to 0.6.1
    
    * Very basic frontend build
    
    * Fix to merge common and vanilla package.json files
    
    * Fix path package.json merge utility
    
    * Downgrade irods to 4.2.1; 4.2.2 has bug/issues with B2SAFE.
    
    * Fixes to work with webpack
    
    * Removed failed tests with no-bin-link option
    
    * [rapydo/issues#90] build for production mode
    
    * A first working image to copy data inside a container cluster from irods
    
    * Adding websocket to read container logs
    
    * Deprecation fix
    
    * Working copy with an irods client container
    
    * [rapydo/issues#83] Add option to set the maintainer email for letsencrypt
    
    * Using root to fix permissions on icommands copy
    
    * update proxy versions
    
    * [rapydo/issues#87] Updating the pretty printer
    
    * Added python3 alias
    
    * Bug fix syntax apt-get
    
    * [rapydo/issues#77] Creation of link to letsencrypt certificate file for neo4j
    
    * Fix to create certificates for neo4j interface
    
    * Fix to create certificates for neo4j interface
    
    * [rapydo/issues#87] fix support for pretty print
    
    * Bump: 0.6.1
    
    * [rapydo/issues#77] BUG fix new certificates for neo4j
    
    * Update nginx
    
    * bug fix
    
    * Bug fix rancher stack conf
    
    * anonymous fix
    
    * [rapydo/issues#94] Developer user is now created by mapping host uid instead of 990
    
    * B2SAFE docker image instructions update
    
    * Installation for bootstrap3, external to npm
    
    * Test email in production
    
    * Talib build
    
    * Adding pip talib and matplotlib
    
    * More compact command
    
    * talib wget is now the first operation, to make retries faster
    
    * Image for testing JSON parameters
    
    * jq fix for seadata
    
    * upload/download icom container
    
    * Removed chown, next version will avoid this by mapping current user on container dev.user
    
    * Changed .coverage file position
    
    * [rapydo/issues#98] Installation of @angular-cli
    
    * Added frontend to tests
    
    * Added frontend to tests
    
    * testing a copy for 0.9 version
    
    * implementing merge of zips strategy in a container
    
    * Set pypi.python.org as trusted host

--- jq/myscript.sh
+++ jq/myscript.sh
@@ -11,4 +11,11 @@ export fname='input.json'
 echo $JSON_INPUT > $fname
 jq -M -S . $fname
 
-sleep 1234567890
+file=$(jq -M -S -r .parameters.batch_number $fname)
+ZIP_FILE="${file}.zip"
+cd $BATCH_DIR_PATH
+echo "unzipping: $ZIP_FILE"
+unzip $file
+
+# sleep 1234567890
+exit 0
