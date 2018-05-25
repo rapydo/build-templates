ca571aebcec0ae8d4c45eecc2f9fd13256b73e36
Mattia <m.dantonio81@gmail.com>
2018-05-25 11:06:31 +0200

    Removed bootstrap3 installation

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -26,20 +26,6 @@ export NODE_PATH=$MODULE_PATH/node_modules
 # cd $MODULE_PATH
 # cd /rapydo
 
-# Only required for Angularjs ... remove me in a near future
-cd $MODULE_PATH
-rm -rf bootstrap3.zip bootstrap-3.3.7-dist
-wget https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip -O bootstrap3.zip
-unzip bootstrap3.zip
-rm -rf bootstrap3.zip 
-cd -
-
-cd $NODE_PATH
-ln -sf ../bootstrap-3.3.7-dist bootstrap3
-cd -
-# ######################################################## #
-
-
 if [ "$APP_MODE" == 'production' ]; then
 
 	exec npm run build 
