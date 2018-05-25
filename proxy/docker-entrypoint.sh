b5bcfa5cc1c4cc6ef36ec724c672a39870850caf
Mattia <m.dantonio81@gmail.com>
2018-05-25 11:07:02 +0200

    Fix permissions to let other services, like neo4j, to write into letsencrypt volume

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -40,6 +40,9 @@ for f in `ls $dedir`; do
     echo
 done
 
+# Let other services, like neo4j, to write into this volume
+chmod -R +w /etc/letsencrypt
+
 #####################
 # Completed
 echo "launching server"
