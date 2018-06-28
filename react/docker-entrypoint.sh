7097e2ac8b187773ebf3ea1e1e26cbfffa8f6a90
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.it>
2018-06-28 23:13:08 +0200

    Fix react image

--- react/docker-entrypoint.sh
+++ react/docker-entrypoint.sh
@@ -7,31 +7,34 @@ set -e
 #
 ######################################
 
-# Defaults
-if [ -z APP_MODE ]; then
-    APP_MODE="debug"
+# # Defaults
+# if [ -z APP_MODE ]; then
+#     APP_MODE="debug"
+# fi
+
+# # Move on build?
+# node /rapydo/utility/merge.js
+# # --production to install only dependencies e not devDependencies
+# npm --prefix $MODULE_PATH install
+# # npm cache clean
+
+# export PATH=$PATH:$MODULE_PATH/node_modules/.bin
+# export NODE_PATH=$MODULE_PATH/node_modules
+
+# if [ "$APP_MODE" == 'production' ]; then
+# 	exec npm run build
+# elif [ "$APP_MODE" == 'debug' ]; then
+# 	exec npm start
+# fi
+
+
+if [ -d "node_modules" ]; then
+    echo "Already built"
+else
+    echo "Building"
+    yarn install
 fi
 
-# Move on build?
-node /rapydo/utility/merge.js
-
-# --production to install only dependencies e not devDependencies
-npm --prefix $MODULE_PATH install
-
-# npm cache clean
-
-export PATH=$PATH:$MODULE_PATH/node_modules/.bin
-export NODE_PATH=$MODULE_PATH/node_modules
-
-# cd $MODULE_PATH
-# cd /rapydo
-
-if [ "$APP_MODE" == 'production' ]; then
-
-	exec npm run build 
-
-elif [ "$APP_MODE" == 'debug' ]; then
-
-	exec npm start 
-
-fi
+echo
+echo "Completed!"
+sleep 1234567890
