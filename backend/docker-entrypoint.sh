fb710f2060bde44620624b8cc8798ae79f9b79cd
Paulie D <p.donoriodemeo@gmail.com>
2018-06-30 00:53:17 +0200

    bug fix on permissions changes

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -28,9 +28,6 @@ if [ ! -f "$secret_file" ]; then
     if [ "$check_volumes" == 'yes' ]; then
         echo "First time access"
 
-        # set permission on the main development folder
-        chown $APIUSER $CODE_DIR
-
         # Create the secret to enable security on JWT tokens
         mkdir -p $JWT_APP_SECRETS
         head -c 24 /dev/urandom > $secret_file
@@ -55,6 +52,9 @@ else
         touch /${JWT_APP_SECRETS}/initialized
     fi
 fi
+
+# fix permissions on the main development folder
+chown $APIUSER $CODE_DIR
 
 #####################
 # Extra scripts
