e8075638232d45d57dc757f6863dc6afb7cabc30
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.it>
2018-07-04 11:00:59 +0200

    Patch to use on DO a react test

--- react/docker-entrypoint.sh
+++ react/docker-entrypoint.sh
@@ -2,32 +2,15 @@
 set -e
 
 ######################################
-#
 # Entrypoint!
-#
 ######################################
 
-# # Defaults
-# if [ -z APP_MODE ]; then
-#     APP_MODE="debug"
-# fi
+# Defaults
+if [ -z APP_MODE ]; then
+    APP_MODE="debug"
+fi
 
-# # Move on build?
-# node /rapydo/utility/merge.js
-# # --production to install only dependencies e not devDependencies
-# npm --prefix $MODULE_PATH install
-# # npm cache clean
-
-# export PATH=$PATH:$MODULE_PATH/node_modules/.bin
-# export NODE_PATH=$MODULE_PATH/node_modules
-
-# if [ "$APP_MODE" == 'production' ]; then
-# 	exec npm run build
-# elif [ "$APP_MODE" == 'debug' ]; then
-# 	exec npm start
-# fi
-
-
+# Install node modules
 if [ -d "node_modules" ]; then
     echo "Already built"
 else
@@ -35,6 +18,13 @@ else
     yarn install
 fi
 
-echo
-echo "Completed!"
-sleep 1234567890
+# What else?
+if [ -d "node_modules" ]; then
+if [ "$APP_MODE" == 'production' ]; then
+  echo "Prod mode: build and serve"
+  exec yarn build && cd build && python3 -m http.server 5000
+else
+# elif [ "$APP_MODE" == 'debug' ]; then
+  echo "Dev mode: sleeping"
+  exec sleep 1234567890
+fi
