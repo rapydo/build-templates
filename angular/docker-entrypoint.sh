40b533ce3837cd3056a0897a3948dfcf9bdaebee
Mattia <m.dantonio81@gmail.com>
2019-09-21 08:10:02 +0200

    Added installation of @angular-cli

--- angular/docker-entrypoint.sh
+++ angular/docker-entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-######################################
-#
-# Entrypoint!
-#
-######################################
+#######################################
+#                                     #
+#             Entrypoint!             #
+#                                     #
+#######################################
 
 # Defaults
 if [ -z APP_MODE ]; then
     APP_MODE="debug"
 fi
 
-# Move on build?
 node /rapydo/utility/merge.js
 
 # --production to install only dependencies e not devDependencies
@@ -22,9 +21,6 @@ npm --prefix $MODULE_PATH install
 
 export PATH=$PATH:$MODULE_PATH/node_modules/.bin
 export NODE_PATH=$MODULE_PATH/node_modules
-
-# cd $MODULE_PATH
-# cd /rapydo
 
 if [ "$APP_MODE" == 'production' ]; then
 
