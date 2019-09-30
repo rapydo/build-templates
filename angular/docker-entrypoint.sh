5d38eae58d376f9d23611ddf6d0dff0727b4ed6c
Mattia <m.dantonio81@gmail.com>
2019-09-30 15:54:26 +0200

    New build to work with angular-cli

--- angular/docker-entrypoint.sh
+++ angular/docker-entrypoint.sh
@@ -12,6 +12,9 @@ if [ -z APP_MODE ]; then
     APP_MODE="debug"
 fi
 
+env > /tmp/.env
+node /rapydo/utility/config-env.ts
+
 node /rapydo/utility/merge.js
 
 # --production to install only dependencies e not devDependencies
