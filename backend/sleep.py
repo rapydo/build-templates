6b561b56c6d69b52e4fbcd1c23648cd52b901432
Mattia <m.dantonio81@gmail.com>
2019-12-02 11:05:17 +0100

    Replaced deprecated get_logger with loguru log

--- backend/sleep.py
+++ backend/sleep.py
@@ -4,9 +4,7 @@
 import signal
 import sys
 import time
-from restapi.utilities.logs import get_logger
-
-log = get_logger(__name__)
+from restapi.utilities.logs import log
 
 
 def signal_term_handler(signal=None, frame=None, name='SIGTERM'):
