9d87fb1d41c5747fc8c0d923d2f989af1df4a3b7
Mattia <m.dantonio81@gmail.com>
2019-12-06 13:20:30 +0100

    Automatic nginx configuration loading based on enabled services

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -6,28 +6,40 @@ TEMPLATE_DIR="/etc/nginx/sites-enabled-templates"
 
 mkdir -p ${CONF_DIR}
 
-CONF_TEMPLATE="${TEMPLATE_DIR}/production"
-HEADERS_TEMPLATE="${TEMPLATE_DIR}/production-headers"
+function convert_conf {
+    CONF_TEMPLATE=$1
+    CONF_FILE=$2
+    if [[ -f "$CONF_TEMPLATE" ]]; then
+        echo "Converting ${CONF_TEMPLATE} into ${CONF_FILE} by replacing env"
+        # We pass to envsubst the list of env variables, to only replace existing variables
+        envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < $CONF_TEMPLATE > $CONF_FILE
+    else
+        echo "${CONF_TEMPLATE} not found"
+    fi
 
-CONF_FILE="${CONF_DIR}/production"
-HEADERS_FILE="${CONF_DIR}/production-headers"
+}
 
-if [[ -f "$CONF_TEMPLATE" ]]; then
-    echo "Converting ${CONF_TEMPLATE} into ${CONF_FILE} by replacing env"
-    # We pass to envsubst the list of env variables, to only replace existing variables
-    envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < $CONF_TEMPLATE > $CONF_FILE
-else
-    echo "${CONF_TEMPLATE} not found"
+# *.conf are loaded from main nginx.conf
+# *.service are loaded from production.conf
+# confs with no extension are loaded from service conf
+
+convert_conf ${TEMPLATE_DIR}/production.conf ${CONF_DIR}/production.conf
+convert_conf ${TEMPLATE_DIR}/backend.conf ${CONF_DIR}/backend.service
+
+# Frontend configuration
+if [[ -f "$TEMPLATE_DIR/${FRONTEND}.conf" ]]; then
+    convert_conf ${TEMPLATE_DIR}/${FRONTEND}.conf ${CONF_DIR}/${FRONTEND}.service
 fi
 
-if [[ -f "$HEADERS_TEMPLATE" ]]; then
-    echo "Converting ${HEADERS_TEMPLATE} into ${HEADERS_FILE} by replacing env"
-    # We pass to envsubst the list of env variables, to only replace existing variables
-    envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')"< $HEADERS_TEMPLATE > $HEADERS_FILE
+# Custom Frontend header configuration, if any
+if [[ -f "$TEMPLATE_DIR/production-${FRONTEND}-headers.conf" ]]; then
+    convert_conf ${TEMPLATE_DIR}/production-${FRONTEND}-headers.conf ${CONF_DIR}/production-headers
 else
-    echo "${HEADERS_TEMPLATE} not found"
+    convert_conf ${TEMPLATE_DIR}/production-headers.conf ${CONF_DIR}/production-headers
 fi
 
+# Extra services....
+# Not implemented
 
 if [ "$1" == 'updatecertificates' ]; then
     if pgrep "nginx" > /dev/null
