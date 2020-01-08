d3d923d35e555519f30d21e2ef13a8dbe642e7aa
Mattia <m.dantonio81@gmail.com>
2020-01-08 12:22:38 +0100

    Added ability to generate self-signed certificates by using rapydo ssl-certificate command on localhost

--- proxy/letsencrypt.sh
+++ proxy/letsencrypt.sh
@@ -22,22 +22,43 @@ if [[ "$1" == "--force" ]]; then
         force="--force"
 fi
 
-./acme.sh --issue ${force} --debug \
-    --fullchain-file ${CERTCHAIN} --key-file ${CERTKEY} \
-    -d $DOMAIN -w $WWWDIR $MODE
 
-if [ "$?" == "0" ]; then
-    # List what we have
-    echo "Completed. Check:"
-    ./acme.sh --list
+if [[ "$DOMAIN" == "localhost" ]]; then
+    # exactly as in docker-entrypoint.sh 
+    echo "Creating a self signed SSL certificate"
+    mkdir -p $CERTDIR/$CERTSUBDIR
+    selfsign
 
-    # Could be executed with acme.sh by using --reloadcmd 'nginx -s reload'
-    nginx -s reload
+    if [ "$?" == "0" ]; then
+        echo "Self signed SSL certificate successfully created"
 
-    cd ${CERTDIR}
-    rm -f neo4j.cert neo4j.key
-    ln -sfn ${CERTCHAINFILE} neo4j.cert
-    ln -sfn ${CERTKEYFILE} neo4j.key
+        cd ${CERTDIR}
+        rm -f neo4j.cert neo4j.key
+        ln -sfn ${CERTCHAINFILE} neo4j.cert
+        ln -sfn ${CERTKEYFILE} neo4j.key
+        chmod +r ${CERTCHAINFILE} ${CERTKEYFILE}
+    else
+        echo "Self signed SSL certificate generation failed!"
+    fi
+
 else
-    echo "ACME FAILED!"
+    ./acme.sh --issue ${force} --debug \
+        --fullchain-file ${CERTCHAIN} --key-file ${CERTKEY} \
+        -d $DOMAIN -w $WWWDIR $MODE
+
+    if [ "$?" == "0" ]; then
+        # List what we have
+        echo "Completed. Check:"
+        ./acme.sh --list
+
+        # Could be executed with acme.sh by using --reloadcmd 'nginx -s reload'
+        nginx -s reload
+
+        cd ${CERTDIR}
+        rm -f neo4j.cert neo4j.key
+        ln -sfn ${CERTCHAINFILE} neo4j.cert
+        ln -sfn ${CERTKEYFILE} neo4j.key
+    else
+        echo "ACME FAILED!"
+    fi
 fi
