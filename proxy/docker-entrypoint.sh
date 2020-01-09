9cc1c2d86a0a037f31d34591ca5ffa2b0a3d47f6
Mattia <m.dantonio81@gmail.com>
2020-01-09 06:58:01 +0100

    Fix to avoid nginx conf to be disrupted by replacing env variabiles containing single quotes

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -19,6 +19,9 @@ function convert_conf {
 
 }
 
+# remove single quotes from these variables to avoid nginx conf to be disrupted
+eval CSP_SCRIPT_SRC=$CSP_SCRIPT_SRC
+eval CSP_IMG_SRC=$CSP_IMG_SRC
 # *.conf are loaded from main nginx.conf
 # *.service are loaded from production.conf
 # confs with no extension are loaded from service conf
