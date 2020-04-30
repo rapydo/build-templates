98c5caa357dd0cfb06003dc65ecac3d9111cc680
Mattia <m.dantonio81@gmail.com>
2020-04-30 08:02:29 +0200

    Temporary disabled uwsgi logs filtering

--- backendirods/launch_uwsgi.sh
+++ backendirods/launch_uwsgi.sh
@@ -34,6 +34,7 @@ echo ${LOGS}
 # These logs are redundant if coupled with normal outputs from flask
 # To make correctly work tail -f with pipe the stdbuf -o0 is needed
 # stdbuf -o0 disables buffering of the output stream and data is output immediately
-tail -n 1000 -f $LOGS | stdbuf -o0 grep -v "^\[pid: [0-9]\+|app: [0-9]\+|req: "
+# tail -n 1000 -f $LOGS | stdbuf -o0 grep -v "^\[pid: [0-9]\+|app: [0-9]\+|req: "
+tail -n 1000 -f $LOGS
 echo "Main WSGI/PROXY logging interrupted!"
 sleep infinity
