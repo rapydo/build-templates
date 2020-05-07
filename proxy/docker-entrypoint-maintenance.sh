b76054d46e2dd39a4ffee8482677b16869123c5f
Mattia <m.dantonio81@gmail.com>
2020-05-07 15:39:28 +0200

    Added a maintenance nginx server to be used as volatile container during long maintenance operations

--- proxy/docker-entrypoint-maintenance.sh
+++ proxy/docker-entrypoint-maintenance.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
+echo -e "\n\n"
+echo "Maintenance server is up and waiting for connections. This server inform users that there is an ongoing maintenance"
+echo "You can turn off this server after the maintenance is completed and before starting the normal stack"
+echo -e "\n\n"
+
 exec nginx -g 'daemon off;'
