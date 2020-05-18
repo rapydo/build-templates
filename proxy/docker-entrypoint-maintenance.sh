721caadb3578c4d5e3b80ca6cf51484820c8a6a4
Mattia <m.dantonio81@gmail.com>
2020-05-18 17:27:48 +0200

    Included nginx confs into the image

--- proxy/docker-entrypoint-maintenance.sh
+++ proxy/docker-entrypoint-maintenance.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+CONF_DIR="/etc/nginx/sites-enabled"
+TEMPLATE_DIR="/etc/nginx/sites-enabled-templates"
+
+cp ${TEMPLATE_DIR}/maintenance.conf ${CONF_DIR}/
+
 echo -e "\n\n"
 echo "Maintenance server is up and waiting for connections. This server inform users that there is an ongoing maintenance"
 echo "You can turn off this server after the maintenance is completed and before starting the normal stack"
