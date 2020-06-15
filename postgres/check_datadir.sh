72db69b8c4b538d61fee624a1883912da3a32860
Mattia <m.dantonio81@gmail.com>
2020-06-15 17:08:50 +0200

    Bug fix to correctly link pg_hba.conf and postgresql.conf files

--- postgres/check_datadir.sh
+++ postgres/check_datadir.sh
@@ -8,6 +8,14 @@ mkdir -p ${DATADIR}
 chown -R postgres ${DATADIR}
 
 # Create current link, if missing
-if [ ! -L $PGDATA ]; then
+if [[ ! -L $PGDATA ]]; then
     ln -sT ${DATADIR} ${PGDATA};
+fi
+
+if [[ -f ${PGDATA}/postgresql.conf ]] && [[ ! -L ${PGDATA}/postgresql.conf ]]; then
+    # Force postgresql.conf in datadir to be a link to the default conf file
+    # Default conf file is copied into the container at build time
+    # This will update alredy-created datadirs.
+    # A similar check in pgs_init.sh will ensure the link for newly created datadirs
+    ln -sf /etc/postgresql/postgresql.conf ${PGDATA}/postgresql.conf
 fi
--- postgres/pgs_init.sh
+++ postgres/pgs_init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -L ${PGDATA}/postgresql.conf ]; then
+if [[ -f ${PGDATA}/postgresql.conf ]] && [[ ! -L ${PGDATA}/postgresql.conf ]]; then
     # Force postgresql.conf in datadir to be a link to the default conf file
     # Default conf file is copied into the container at build time
+    # This will update newly-created datadirs.
+    # A similar check in check_datadir.sh will ensure the link for already created datadirs
     ln -sf /etc/postgresql/postgresql.conf ${PGDATA}/postgresql.conf
 fi
-
 ## http://www.postgresql.org/docs/9.1/static/auth-pg-hba-conf.html
 net="0.0.0.0/0"
 
