a35499e9f15d10a8beca87c660e22a80e21993ab
Mattia <m.dantonio81@gmail.com>
2020-07-28 19:50:08 +0200

    Bug fix to prevent to quote UNSAFE_EVAL variable if empty

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -39,8 +39,11 @@ function convert_conf {
 export CSP_SCRIPT_SRC=${CSP_SCRIPT_SRC//\'/}
 export CSP_IMG_SRC=${CSP_IMG_SRC//\'/}
 export CSP_FONT_SRC=${CSP_FONT_SRC//\'/}
-# add single quotes (this command also works if the string is already single quoted)
-export UNSAFE_EVAL="'${UNSAFE_EVAL//\'/}'"
+# add single quotes if non empty
+# this command also works if the string is already single quoted
+if [[ ! -z "$UNSAFE_EVAL" ]]; then
+    export UNSAFE_EVAL="'${UNSAFE_EVAL//\'/}'"
+fi
 # *.conf are loaded from main nginx.conf
 # *.service are loaded from production.conf
 # confs with no extension are loaded from service conf
