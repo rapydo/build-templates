d6af973cfa1bbacf3cae850e8b354829166f6648
Mattia <m.dantonio81@gmail.com>
2020-09-07 16:57:39 +0200

    # Minor fix

--- angular/reload-types.sh
+++ angular/reload-types.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Converting TS Interfaces to JSON Schemas..."
+
 ts-json-schema-generator -f /app/tsconfig.app.json -p /app/app/rapydo/app/types.ts -o /app/app/types.json
+
+echo "Conversion completed"
