2ecfa21156d3fa52ff0d088254523a11bfe6e6c2
Mattia <m.dantonio81@gmail.com>
2020-09-17 13:59:49 +0200

    # Bug fix

--- angular/reload-types.sh
+++ angular/reload-types.sh
@@ -4,7 +4,7 @@ set -e
 echo "Converting TS Interfaces to JSON Schemas..."
 
 cat /app/app/rapydo/app/types.ts > /tmp/types.ts
-cat /app/app/custom/app/types.ts >> /tmp/types.ts
+cat /app/app/custom/app/types.ts | grep -v 'from "@rapydo/types"' >> /tmp/types.ts
 
 ts-json-schema-generator -f /app/tsconfig.app.json -p /tmp/types.ts -o /app/app/types.json
 
