6d216bf62c8baed91a96c15f396457b5140738cd
Mattia <m.dantonio81@gmail.com>
2020-12-19 18:56:39 +0100

    # Added execution of compile_validation_schemas in reload types

--- angular/reload-types.sh
+++ angular/reload-types.sh
@@ -8,4 +8,6 @@ grep -v 'from "@rapydo/types"' /app/app/custom/app/types.ts >> /tmp/types.ts
 
 ts-json-schema-generator -f /app/tsconfig.app.json -p /tmp/types.ts -o /app/app/types.json
 
+node /rapydo/compile_validation_schemas.js
+
 echo "Conversion completed"
