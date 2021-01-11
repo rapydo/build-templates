df7759534ba7691b28dd655599a2631c58e93c26
Mattia <m.dantonio81@gmail.com>
2021-01-11 08:06:42 +0100

    # Bug fix in creating mongodb user for db celery

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -7,16 +7,21 @@ mongo -- "${MONGO_INITDB_DATABASE}" <<EOF
         {
             user: user,
             pwd: passwd,
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "${MONGO_INITDB_DATABASE}"
-                },
-                {
-                    role: "readWrite",
-                    db: "celery"
-                }
-            ]
+            roles: ["readWrite"]
+        }
+    );
+EOF
+
+mongo -- "celery" <<EOF
+    var user = '${MONGO_INITDB_ROOT_USERNAME}';
+    var passwd = '${MONGO_INITDB_ROOT_PASSWORD}';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(user, passwd);
+    db.createUser(
+        {
+            user: user,
+            pwd: passwd,
+            roles: ["readWrite"]
         }
     );
 EOF
