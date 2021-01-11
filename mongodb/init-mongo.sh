e9dfaae5eb2a30796e9cb6d9702981941842a6ee
Mattia <m.dantonio81@gmail.com>
2021-01-11 15:15:56 +0100

    # MongoDB roles fix /4

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -1,4 +1,19 @@
+# mongo -- "${MONGO_INITDB_DATABASE}" <<EOF
+#     var user = '${MONGO_INITDB_ROOT_USERNAME}';
+#     var passwd = '${MONGO_INITDB_ROOT_PASSWORD}';
+#     var admin = db.getSiblingDB('admin');
+#     admin.auth(user, passwd);
+#     use admin;
+#     db.updateUser(
+#         user,
+#         {
+#             roles: ["dbAdminAnyDatabase"]
+#         }
+#     );
+# EOF
+
 # userAdminAnyDatabase needed to allow dropDatabase
+# roles: ["readWrite"] is not enough
 
 mongo -- "${MONGO_INITDB_DATABASE}" <<EOF
     var user = '${MONGO_INITDB_ROOT_USERNAME}';
