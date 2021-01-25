9d443af4f1adf4efbeb302be552707bd0bd89bab
Mattia <m.dantonio81@gmail.com>
2021-01-25 09:44:10 +0100

    # Bug fix

--- swaggerui/swagger-nginx-prerun.sh
+++ swaggerui/swagger-nginx-prerun.sh
@@ -6,9 +6,9 @@ set -o allexport
 # We pass to envsubst the list of env variables, to only replace existing variables
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < /etc/nginx/adminer-${APP_MODE}.conf > /etc/nginx/nginx.conf
 
-if [[ ${APP_MODE} = "production"} ]];
+if [[ ${APP_MODE} == "production"} ]];
 then
-    export URLS="[ { url: 'https://${DOMAIN}/api/specs', name: 'CustomSpecs' } ]"
+    export URLS="[ { url: 'https://${DOMAIN}/api/specs', name: '${PROJECT_TITLE}' } ]"
 else
-    export URLS="[ { url: 'http://${DOMAIN}:${BACKEND_PORT}/api/specs', name: 'CustomSpecs' } ]"
+    export URLS="[ { url: 'http://${DOMAIN}:${BACKEND_PORT}/api/specs', name: '${PROJECT_TITLE}' } ]"
 fi
