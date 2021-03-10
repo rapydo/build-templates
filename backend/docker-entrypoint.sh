7ce0dbb6b0ba88c14a1c471057f9282671b91607
Mattia <m.dantonio81@gmail.com>
2021-03-10 11:52:39 +0100

    # JWT_APP_SECRETS -> APP_SECRETS

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-DEVID=$(id -u $APIUSER)
-if [ "$DEVID" != "$CURRENT_UID" ]; then
-    echo "Fixing uid of user $APIUSER from $DEVID to $CURRENT_UID..."
-    usermod -u $CURRENT_UID $APIUSER
+DEVID=$(id -u ${APIUSER})
+if [ "${DEVID}" != "${CURRENT_UID}" ]; then
+    echo "Fixing uid of user ${APIUSER} from ${DEVID} to ${CURRENT_UID}..."
+    usermod -u ${CURRENT_UID} ${APIUSER}
 fi
 
-GROUPID=$(id -g $APIUSER)
-if [ "$GROUPID" != "$CURRENT_GID" ]; then
-    echo "Fixing gid user $APIUSER from $GROUPID to $CURRENT_GID..."
-    groupmod -og $CURRENT_GID $APIUSER
+GROUPID=$(id -g ${APIUSER})
+if [ "${GROUPID}" != "${CURRENT_GID}" ]; then
+    echo "Fixing gid user ${APIUSER} from ${GROUPID} to ${CURRENT_GID}..."
+    groupmod -og ${CURRENT_GID} ${APIUSER}
 fi
 
 # Defaults
@@ -19,26 +19,26 @@ if [ -z APP_MODE ]; then
 fi
 
 # INIT if necessary
-secret_file="$JWT_APP_SECRETS/secret.key"
-init_file="${JWT_APP_SECRETS}/initialized"
+secret_file="${APP_SECRETS}/secret.key"
+init_file="${APP_SECRETS}/initialized"
 
 if [ ! -f "$secret_file" ]; then
 
     # Create the secret to enable security on JWT tokens
-    # mkdir -p $JWT_APP_SECRETS
+    # mkdir -p $APP_SECRETS
     # head -c 24 /dev/urandom > $secret_file
 
-    chown ${APIUSER} ${JWT_APP_SECRETS}
+    chown ${APIUSER} ${APP_SECRETS}
     # certificates chains for external oauth services (e.g. B2ACCESS)
     update-ca-certificates
 fi
 
-if [ ! -f "$init_file" ]; then
+if [ ! -f "${init_file}" ]; then
     echo "Init flask app"
-    HOME=$CODE_DIR su -p $APIUSER -c 'restapi init --wait'
+    HOME=${CODE_DIR} su -p ${APIUSER} -c 'restapi init --wait'
     if [ "$?" == "0" ]; then
         # Sync after init with compose call from outside
-        touch $init_file
+        touch ${init_file}
     else
         echo "Failed to startup flask!"
         exit 1
@@ -46,7 +46,7 @@ if [ ! -f "$init_file" ]; then
 fi
 
 # fix permissions on the main development folder
-chown $APIUSER $CODE_DIR
+chown ${APIUSER} ${CODE_DIR}
 
 #####################
 # Extra scripts
@@ -62,8 +62,8 @@ chown $APIUSER $CODE_DIR
 #####################
 # Fixers: part 1
 
-if [ -d "$CERTDIR" ]; then
-    chown -R $APIUSER $CERTDIR
+if [ -d "${CERTDIR}" ]; then
+    chown -R ${APIUSER} ${CERTDIR}
 fi
 
 #####################
@@ -71,7 +71,7 @@ fi
 export CONTAINER_ID=$(head -1 /proc/self/cgroup|cut -d/ -f3 | cut -c1-12)
 export IS_CELERY_CONTAINER=0
 
-if [[ "$CRONTAB_ENABLE" == "1" ]]; then
+if [[ "${CRONTAB_ENABLE}" == "1" ]]; then
     if [[ "$(find /etc/cron.rapydo/ -name '*.cron')" ]]; then
         echo "Enabling cron..."
 
@@ -79,7 +79,7 @@ if [[ "$CRONTAB_ENABLE" == "1" ]]; then
         env | sed "s/'/\\'/" | sed "s/=\(.*\)/='\1'/" > /etc/rapydo-environment
 
         touch /var/log/cron.log
-        chown $APIUSER /var/log/cron.log
+        chown ${APIUSER} /var/log/cron.log
         # Adding an empty line to cron log
         echo "" >> /var/log/cron.log
         cron
@@ -105,7 +105,7 @@ else
     ## NORMAL MODES
     echo "REST API backend server is ready to be launched"
 
-    if [[ $ALEMBIC_AUTO_MIGRATE == "1" ]] && [[ ${AUTH_SERVICE} == "sqlalchemy" ]]; then
+    if [[ ${ALEMBIC_AUTO_MIGRATE} == "1" ]] && [[ ${AUTH_SERVICE} == "sqlalchemy" ]]; then
 
         if [[ ! -d "${VANILLA_PACKAGE}/migrations" ]]; then
             echo "Skipping migrations check, ${VANILLA_PACKAGE}/migrations does not exist";
@@ -121,16 +121,16 @@ else
 
     fi
 
-    if [ "$APP_MODE" == 'production' ]; then
+    if [ "${APP_MODE}" == 'production' ]; then
 
         echo "waiting for services"
-        HOME=$CODE_DIR su -p $APIUSER -c 'restapi wait'
+        HOME=$CODE_DIR su -p ${APIUSER} -c 'restapi wait'
 
         echo "ready to launch production gunicorn+meinheld"
         mygunicorn
 
     else
-        # HOME=$CODE_DIR su -p $APIUSER -c 'restapi launch'
+        # HOME=$CODE_DIR su -p ${APIUSER} -c 'restapi launch'
         echo "Development mode"
     fi
 
