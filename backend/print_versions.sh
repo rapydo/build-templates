ec00c08cf346505b5388610aa0f5ba187d3181f8
Mattia <m.dantonio81@gmail.com>
2021-04-24 11:59:21 +0200

    # Splash screens fixes

--- backend/print_versions.sh
+++ backend/print_versions.sh
@@ -4,3 +4,4 @@ if [[ ${IS_CELERY_CONTAINER} ]]; then
     echo "$(python3 --version) - Celery $(celery --version)"
 else
     echo "$(python3 --version) - $(flask --version | grep Flask)"
+fi
--- rabbitmq/enable-plugins.sh
+++ rabbitmq/enable-plugins.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -eu
 
-if [[ ! -t 0 ]]; then
-    /bin/bash /etc/banner.sh
-fi
-
 if [[ $RABBITMQ_ENABLE_SHOVEL_PLUGIN == 1 ]];
 then
 	rabbitmq-plugins enable rabbitmq_shovel rabbitmq_shovel_management
