bfb3ad0b422dae0a9a1d9b24a727ea917ba12f6f
Mattia <m.dantonio81@gmail.com>
2021-04-24 13:26:09 +0200

    # Splash screens fixes

--- backend/print_versions.sh
+++ backend/print_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # even if !/bin/bash, it will be executed as /bin/sh, do not use [[ ... ]]
-if [ ${HOSTNAME} == "backend-server" ]; then
+if [ ${HOSTNAME} = "backend-server" ]; then
     echo "$(python3 --version) - $(flask --version | grep Flask)"
 else
     echo "$(python3 --version) - Celery $(celery --version)"
