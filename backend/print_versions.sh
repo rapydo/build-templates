6345f70a4cd1b4a4418137485f8317d90e99c6aa
Mattia <m.dantonio81@gmail.com>
2021-04-24 12:07:43 +0200

    # Splash screens fixes

--- backend/print_versions.sh
+++ backend/print_versions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ ${IS_CELERY_CONTAINER} ]]; then
+# even if !/bin/bash, it will be executed as /bin/sh, do not use [[ ... ]]
+if [ ${IS_CELERY_CONTAINER} ]; then
     echo "$(python3 --version) - Celery $(celery --version)"
 else
     echo "$(python3 --version) - $(flask --version | grep Flask)"
