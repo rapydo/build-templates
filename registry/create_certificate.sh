f9b8da2ad5814229d6d1a5ee5e6bfc370a571d43
Mattia <m.dantonio81@gmail.com>
2021-06-06 10:58:53 +0200

    # Bug fix in tsl generation on registry image

--- registry/create_certificate.sh
+++ registry/create_certificate.sh
@@ -7,10 +7,15 @@ then
     exit 1
 fi
 
-# REGISTRY_ADDRESS == manager.host:port/
-# cut everything after the : => subjectAltName == manager.host
-subjectAltName=$(echo ${REGISTRY_ADDRESS%:*})
+if [[ ! -f ${REGISTRY_HTTP_TLS_KEY} || ! -f ${REGISTRY_HTTP_TLS_CERTIFICATE} ]];
+then
+    # REGISTRY_ADDRESS == manager.host:port/
+    # cut everything after the : => ADDRESS == manager.host
+    ADDRESS=$(echo ${REGISTRY_ADDRESS%:*})
 
-echo -e "basicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=$subjectAltName" > /tmp/config.ini
+    echo -e "[ req ]\ndistinguished_name  = req_distinguished_name\n\n[ req_distinguished_name ]\ncountryName = XX\n\nbasicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=$ADDRESS" > /tmp/config.ini
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_HTTP_TLS_KEY} -x509 -days 365 -config /tmp/config.ini -out ${REGISTRY_HTTP_TLS_CERTIFICATE}
+    cat /tmp/config.ini
+
+    openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_HTTP_TLS_KEY} -x509 -days 365 -config /tmp/config.ini -out ${REGISTRY_HTTP_TLS_CERTIFICATE} -subj "/CN=${ADDRESS}"
+fi
