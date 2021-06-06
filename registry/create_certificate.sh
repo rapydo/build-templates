21bd9fac61014256c9b1045a8ef9c6fff4ef7cef
Mattia <m.dantonio81@gmail.com>
2021-06-06 11:41:37 +0200

    # Bug fix to generate tls certificates with ip addresses as san

--- registry/create_certificate.sh
+++ registry/create_certificate.sh
@@ -13,7 +13,9 @@ then
     # cut everything after the : => ADDRESS == manager.host
     ADDRESS=$(echo ${REGISTRY_ADDRESS%:*})
 
-    echo -e "[ req ]\ndistinguished_name  = req_distinguished_name\n\n[ req_distinguished_name ]\ncountryName = XX\n\nbasicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=$ADDRESS" > /tmp/config.ini
+    # Beware! This only works if ADDRESS is an IP address.
+    # Otherwise remove "IP:" in the subjectAltName
+    echo -e "[ req ]\ndistinguished_name  = req_distinguished_name\n\n[ req_distinguished_name ]\ncountryName = XX\n\nbasicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=IP:$ADDRESS" > /tmp/config.ini
 
-    openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_HTTP_TLS_KEY} -x509 -days 365 -config /tmp/config.ini -out ${REGISTRY_HTTP_TLS_CERTIFICATE} -subj "/CN=${ADDRESS}"
+    openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_HTTP_TLS_KEY} -x509 -days 365 -config /tmp/config.ini -out ${REGISTRY_HTTP_TLS_CERTIFICATE} -subj '/CN=*/'
 fi
