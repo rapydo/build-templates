a08b7fc4c63f7853dcd0a3e8a0ee0c5070143297
Mattia <m.dantonio81@gmail.com>
2021-06-06 14:43:00 +0200

    # Bug fix to generate tls certificates with ip addresses as san

--- registry/create_certificate.sh
+++ registry/create_certificate.sh
@@ -13,8 +13,27 @@ then
     # cut everything after the : => ADDRESS == manager.host
     ADDRESS=$(echo ${REGISTRY_ADDRESS%:*})
 
-    # Beware! This only works if ADDRESS is an IP address.
-    # Otherwise remove "IP:" in the subjectAltName
-    echo -e "[req]\ndistinguished_name  = req_distinguished_name\n\n[req_distinguished_name]\ncountryName = XX\n\n[v3_ca]\nbasicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=@alt_names\n\n[alt_names]\nIP.1 = $ADDRESS" > /tmp/config.ini
+    echo -e """
+[req]
+default_bits = 4096
+default_md = sha256
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
+prompt = no
+[req_distinguished_name]
+C = XX
+ST = XX
+L = XXX
+O = NoCompany
+OU = Orgainizational_Unit
+CN = ${$ADDRESS}
+[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = ${$ADDRESS}
+"""
+ > /tmp/config.ini
     openssl req -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_HTTP_TLS_KEY} -x509 -days 365 -config /tmp/config.ini -out ${REGISTRY_HTTP_TLS_CERTIFICATE} -subj '/CN=*/'
 fi
