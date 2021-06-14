6010813dcb26c53a0991623dc0b796bd12b294d8
Mattia <m.dantonio81@gmail.com>
2021-06-14 17:43:53 +0200

    Adminer and swagger prod conf fixes

--- swaggerui/swagger-nginx-prerun.sh
+++ swaggerui/swagger-nginx-prerun.sh
@@ -8,7 +8,7 @@ if [[ ! -t 0 ]]; then
 fi
 
 # We pass to envsubst the list of env variables, to only replace existing variables
-envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < /etc/nginx/adminer-${APP_MODE}.conf > /etc/nginx/nginx.conf
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < /etc/nginx/swaggerui-${APP_MODE}.conf > /etc/nginx/nginx.conf
 
 if [[ ${APP_MODE} == "production" ]];
 then
