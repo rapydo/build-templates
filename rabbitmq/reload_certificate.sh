50247334a235d0ce196a474a840dc4be1bc7b8cf
Mattia <m.dantonio81@gmail.com>
2021-06-30 17:21:58 +0200

    # command fix

--- rabbitmq/reload_certificate.sh
+++ rabbitmq/reload_certificate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-export RABBITMQ_CTL_ERL_ARGS=""
-
-rabbitmqctl eval 'ssl:clear_pem_cache().'
+RABBITMQ_CTL_ERL_ARGS="" rabbitmqctl eval 'ssl:clear_pem_cache().'
 
 echo "PEM Cached cleared"
-
