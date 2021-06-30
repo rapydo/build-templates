3c58edad9a25726c8a7296804699bf7867b384c2
Mattia <m.dantonio81@gmail.com>
2021-06-30 18:49:26 +0200

    Undo previous commit (Removed rabbitmq/reload_certificate.sh script) -> script restored

---
+++ rabbitmq/reload_certificate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [[ $(rabbitmq-diagnostics status) ]];
+then
+    rabbitmqctl eval 'ssl:clear_pem_cache().'
+
+    echo "PEM Cached cleared"
+else
+    echo "RabbitMQ is not reachable (still starting?), can't clear the PEM cache"
+fi
+
