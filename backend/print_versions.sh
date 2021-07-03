252fb9c673189510298786420d2950203f54b568
Mattia <m.dantonio81@gmail.com>
2021-07-03 08:16:55 +0200

    # Bug fix

--- backend/print_versions.sh
+++ backend/print_versions.sh
@@ -4,6 +4,7 @@
 if [ ${HOSTNAME} = "backend-server" ]; then
     echo "$(python3 --version) - $(flask --version | grep Flask)"
 elif [ ${HOSTNAME} = "flower" ]; then
-else
     echo "$(python3 --version) - Celery $(celery --version) - Flower $(pip3 list | grep flower | awk {'print $2'})"
+else
+    echo "$(python3 --version) - Celery $(celery --version)"
 fi
