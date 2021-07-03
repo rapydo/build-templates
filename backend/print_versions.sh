0f988ba092b685ec0e8bd49abbde1c7478bc961b
Mattia <m.dantonio81@gmail.com>
2021-07-03 07:34:18 +0200

    Added Flower version to the banner

--- backend/print_versions.sh
+++ backend/print_versions.sh
@@ -3,6 +3,7 @@
 # even if !/bin/bash, it will be executed as /bin/sh, do not use [[ ... ]]
 if [ ${HOSTNAME} = "backend-server" ]; then
     echo "$(python3 --version) - $(flask --version | grep Flask)"
+elif [ ${HOSTNAME} = "flower" ]; then
 else
-    echo "$(python3 --version) - Celery $(celery --version)"
+    echo "$(python3 --version) - Celery $(celery --version) - Flower $(pip3 list | grep flower | awk {'print $2'})"
 fi
