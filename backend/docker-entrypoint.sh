0704a8915ed292222d08436b5bd23b6cb5606efb
Mattia <m.dantonio81@gmail.com>
2021-09-19 12:39:38 +0200

    Alchemy migrations are now executed at user level

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -88,7 +88,7 @@ else
 
             # Short version: flask_migrate upgrade
             # Please note that errors in the upgrade will not make fail the server startup due to the || true statement
-            flask db upgrade --directory "${PROJECT_NAME}/migrations" || true;
+            HOME=$CODE_DIR su -p ${APIUSER} -c 'flask db upgrade --directory "${PROJECT_NAME}/migrations" || true';
 
             echo "Migration completed";
         fi
