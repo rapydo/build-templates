8f4643d9d32f962dbb470b78c0dd018ecbd9dbfc
Mattia <m.dantonio81@gmail.com>
2021-10-25 19:57:40 +0200

    Fixed admine and swaggerui builds in testing mode

--- swaggerui/swagger-nginx-prerun.sh
+++ swaggerui/swagger-nginx-prerun.sh
@@ -7,6 +7,10 @@ if [[ ! -t 0 ]]; then
     /bin/ash /etc/banner.sh
 fi
 
+if [[ "${APP_MODE}" == "test" ]]; then
+    APP_MODE="development"
+fi
+
 # We pass to envsubst the list of env variables, to only replace existing variables
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < /etc/nginx/swaggerui-${APP_MODE}.conf > /etc/nginx/nginx.conf
 
