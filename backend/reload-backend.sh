3bfdaf73e935eea88a4d48a430d05965d49447ab
Mattia <m.dantonio81@gmail.com>
2021-10-29 08:36:05 +0200

    Added backend reload script

--- backend/reload-backend.sh
+++ backend/reload-backend.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Not implemented yet"
+if [ "$APP_MODE" == "production" ]; then
+    # Can't use pidof because it is execute via python
+    PID=$(pgrep gunicorn | head -1)
+    echo "Reloading gunicorn (PID #${PID})..."
+    kill -s SIGHUP ${PID}
+else
+
+    echo "Reloading Flask..."
+    touch ${PYTHON_PATH}/restapi/__main__.py
+fi
