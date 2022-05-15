5b193554cee19020a102ed7555b632b9d8c3efba
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-05-15 08:10:44 +0200

    [fail2ban] Enabled custom configurations for iptables/nftables

--- fail2ban/fix-configuration.sh
+++ fail2ban/fix-configuration.sh
@@ -3,9 +3,11 @@
 sed -i "s|#ignoreip =.*|ignoreip = 127.0.0.1/24 ${DOCKER_SUBNET}|g" /etc/fail2ban/jail.conf
 
 if [ "$FAIL2BAN_IPTABLES" == "nf_tables" ]; then
+    echo "Enabling nftables-allport rules"
     sed -i "s|banaction =.*|banaction = nftables-allports|g" /etc/fail2ban/jail.conf
+    cp /data/action.d.available/nftables-common.local /data/action.d/
 elif [ "$FAIL2BAN_IPTABLES" == "legacy" ]; then
+    echo "Enabling iptables-allport rules"
     sed -i "s|banaction =.*|banaction = iptables-allports|g" /etc/fail2ban/jail.conf
+    cp /data/action.d.available/nftables-common.local /data/action.d/
 fi
-
-banaction = iptables-allports
